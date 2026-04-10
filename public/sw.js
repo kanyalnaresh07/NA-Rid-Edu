344413fb610078c879dc438e2f343b0df0a97ed0
kanyalnaresh07 <kanyal.naresh07@gmail.com>
2026-04-10 21:46:29 +0530

    feat: Update service worker cache strategy
    Refactors the service worker's caching mechanism to improve performance and maintainability. It now caches essential application shell assets and implements a robust cache invalidation strategy for updates.

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,28 @@
-const CACHE_NAME = "narid-cache-v1";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
+const CACHE_NAME = "narid-v1";
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/manifest.json"
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
 });
